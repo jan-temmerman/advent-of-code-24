1bc96f73fd57ba8b4941ac3ca6157607b7887e63
jantemmerman <temmjan@gmail.com>
2024-12-03 23:09:06 +0100

    advent of code day 1

--- solutions/day1/1.1.js
+++ solutions/day1/1.1.js
@@ -2,7 +2,7 @@ import {getSanitizedInput} from "./util.js";
 
 const [col1, col2] = getSanitizedInput();
 
-// Calculate the absolute difference between the 2 columns and sum them
+// Calculate the sum of the products of every value from col1 and the amount of times it occurs in col2
 const result = col1.reduce((acc, v1, i) => {
     const multiplier = col2.filter(v2 => v2 === v1).length
     acc+= v1*multiplier;
